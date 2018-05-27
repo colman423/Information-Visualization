ae381275ce49bd8f40ecc0b44de6758c79a3122c
colman423 <colman0423@gmail.com>
2018-05-27 23:29:02 +0800

    update hard code

--- news/final/index.js
+++ news/final/index.js
@@ -11,20 +11,20 @@ var svg = d3.select('#svg');
 var xLine = svg.append("g").attr("class", "x axis").attr("transform", `translate(${margin.left}, ${height+margin.top})`);
 var yLine = svg.append("g").attr("class", "y axis").attr("transform", `translate(${margin.left}, ${margin.top})`);
 var tip = d3.tip().attr('class', 'd3-tip').offset([-10, 0]).html(function(d) {
-    return `<strong>${d.category} ${d.quota}</strong>`;
+    return `<strong>${d.name} ${d.value} ${d.sum}</strong>`;
 });
 svg.call(tip);
 
 var xScale = d3.scale.ordinal()
-.domain([103, 104, 105, 106, 107, "sum"])
+.domain([103, 104, 105, 106, 107])
 .rangeRoundBands([0, width]);
 
 var yScale = d3.scale.linear()
-.domain([0,2000])
+.domain([0,5000])
 .range([0, height])
 
 var y = d3.scale.linear()
-.domain([0,2000])
+.domain([0,5000])
 .range([height, 0]);
 
 var colors = d3.scale.category10();
@@ -39,44 +39,33 @@ yLine.call(function(g){
     g.call(axis);
 });
 
-d3.json("./quota.json", function(err, data) {
+d3.json("http://ghost.cs.nccu.edu.tw/~s10329/vis/news/quota.json", function(err, data) {
+    // console.log(err);
     console.log(data);
-
-
-    for( let i=0; i<data.length; i++ ) {
-        let item = data[i];
-        let label = item['year'];
-        let g = svg.append('g').attr('class', 'bar');    //year bar
-
-        let prevHeight = 0;
-
-        g.selectAll('rect')
-        .data(item.data)
-        .enter()
-        .append('rect')
-        .attr({
-            'x': function() {
-                return xScale(label) + margin.left + margin.right;
-            },
-            'y': function(d) {
-                console.log(d);
-                console.log(prevHeight);
-                return y(d.quota) + margin.top - prevHeight;
-            },
-            'id': function(d) {
-                return d.category+"-"+d.quota;
-            },
-            'width': 105,
-            'height': function(d){
-                let h = yScale(d.quota);
-                prevHeight += h;
-                return h;
-            },
-            'fill': function(d, i){return colors(i)},
-        })
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide);
-        // }
-    }
-
+    let g = svg.append('g').attr('class', 'bar');
+    g.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr({
+        'x': function(d) {
+            return xScale(d.year) + margin.left + margin.right;
+        },
+        'y': function(d) {
+            return y(d.sum) + margin.top;
+        },
+        'id': function(d) {
+            return d.name;
+        },
+        'width': 105,
+        'height': function(d) {
+            return yScale(d.value);
+        },
+        'fill': function(d, i){return colors(d.name)}
+    })
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide)
+    .on('click', function(d) {
+        console.log(d);
+    })
 });
